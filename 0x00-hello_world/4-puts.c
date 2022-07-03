c9dfa20c71b738d0a9aca01a978ec0fd9681d54c
ibrezma <imw31691@gmail.com>
2022-07-02 19:07:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,22 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
- *main - entry point
  *
- *Return: Always 0
- */
+ *  *main - entry point
+ *
+ *   *
+ *
+ *    *Return: 0 after printing the function
+ *
+ *     */
+
 int main(void)
+
 {
-puts ("/"Programming is like building a multilingual puzzle");
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 
 }
