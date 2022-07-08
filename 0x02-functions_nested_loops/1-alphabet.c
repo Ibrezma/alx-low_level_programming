cdb739ce53048213d0b3a51e08843f4779565627
ibrezma <imw31691@gmail.com>
2022-07-08 12:02:11 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-
-	_putchar (i);
-	
+		_putchar(i);
 	}
+	_putchar("\n")
 }
