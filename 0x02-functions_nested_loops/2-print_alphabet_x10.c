f4e2a1148a3b010633d953d8e779db98b485e48d
ibrezma <imw31691@gmail.com>
2022-07-08 12:41:55 -0700

    print 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - print 1 to 0 ten times
+ * Return: returns void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= z; j++)
+		{
+			putchar(j);
+		}
+		putchar('\n');
+	}
+}
