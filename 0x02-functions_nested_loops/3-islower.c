b99bf9525041d39bcacf3299106529906c42a1d6
ibrezma <imw31691@gmail.com>
2022-07-08 13:05:49 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks whether input is lower or upper
+ * @c: input to be checked
+ * Return: returns 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
