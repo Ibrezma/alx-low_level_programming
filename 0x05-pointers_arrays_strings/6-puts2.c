e0986f56d3ba38f6dcd13aa0d5570befc48ee9ea
ibrezma <imw31691@gmail.com>
2022-07-12 12:59:38 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts2 - prints every other character from 0
+ * @str: string to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (len % 2 == 0)
+		{
+			putchar(str[len]);
+		}
+		len++;
+	}
+	putchar('\n');
+}
