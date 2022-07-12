46ffe7164d5b03ed7d348a6bf89115c910e4b395
ibrezma <imw31691@gmail.com>
2022-07-12 12:42:11 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * rev_string - prints the reverse of the input
+ * @s: input to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+
+	int i;
+
+	int len;
+
+	int len2;
+
+	int temp;
+
+	len = strlen(s);
+	len2 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len2];
+		s[len2] = temp;
+		len2--;
+	}
+}
