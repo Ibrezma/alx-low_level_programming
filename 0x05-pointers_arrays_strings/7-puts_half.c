25896ba389b11e515ddff897ce2f1a814fc4a6ec
ibrezma <imw31691@gmail.com>
2022-07-12 13:24:03 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - prints half string
+ * @str: string to be halved
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len;
+
+	int sec_half;
+
+	if (len % 2 == 0)
+	{
+		sec_half = len / 2;
+	}
+	else
+	{
+		sec_half = (len + 1) / 2;
+	}
+	while (str[sec_half] != '\0')
+	{
+		putchar(str[sec_half]);
+	}
+	putchar('\n');
+}
