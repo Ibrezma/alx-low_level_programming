972828b2d8ee821960ad9853f3f3356604a1e23a
ibrezma <imw31691@gmail.com>
2022-07-12 11:27:19 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string to the stdout
+ * @str: string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+	printf("%s\n", str);
+}
