4a549b181e3ec8ce1f80412c8c7a816a56df757e
ibrezma <imw31691@gmail.com>
2022-07-14 08:16:40 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lower case
+ * Return: returns 0 always (success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
