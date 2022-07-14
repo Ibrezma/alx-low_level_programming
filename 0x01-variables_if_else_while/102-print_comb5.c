7c401694e5d6a27dfff30861473ba253d0421bd3
ibrezma <imw31691@gmail.com>
2022-07-14 10:01:10 -0700

    11

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/**
+ *  * main - main block
+ *   * program that prints all possible combinations of two two-digit numbers
+ *    * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	int j;
+
+	int a;
+
+	int b;
+
+	int c;
+
+	int d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10;
+
+		b = i % 10;
+
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10;
+
+			d = j % 10;
+
+			if (a < c || (a == c && b < d))
+			{
+					putchar(a + '0');
+
+					putchar(b + '0');
+
+					putchar(32);
+
+					putchar(c + '0');
+
+					putchar(d + '0');
+
+			if (!(a == 9 && b == 8))
+			{
+				putchar(44);
+				putchar(32);
+
+			}
+
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
