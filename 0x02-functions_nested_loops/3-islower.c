365cd80968a4d743c933adfc523e6ebe8f409b3c
ibrezma <imw31691@gmail.com>
2022-07-14 10:29:49 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * _islower - checks whether input is lower or upper
  * @c: input to be checked
  * Return: returns 1 or 0
  */
-int test _islower(int c)
+int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
+	if (c > 96 && c < 123)
 		return (1);
-	}
 	else
-	{
-	return (0);
-	}
-	_putchar('\n')
+		return (0);
 }
