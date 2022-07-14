f683403adb435f60400ce14bb6cf0973227eedf7
ibrezma <imw31691@gmail.com>
2022-07-14 11:38:04 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+/**
+ *  * _strcmp - compaires two string
+ *   * @s1: first string
+ *    * @s2: second string
+ *     * Return: returns diff of the two
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	char *i;
+	char *j;
+
+	i = s2;
+	j = s1;
+
+	while (*s1 != '\0' && *s2 != '\0' && *j == *i)
+	{
+		j++;
+		i++;
+	}
+	return (*j - *i);
+
+}
