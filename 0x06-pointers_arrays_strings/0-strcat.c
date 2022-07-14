3837f8d22fb573d360e604e9fe0d028d564c72a7
ibrezma <imw31691@gmail.com>
2022-07-14 10:46:11 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ *  * _strcat - concatinate two string
+ *   * @dest: destination string
+ *    * @src: source string
+ *     * Return: returns the concatineted string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int end;
+	int i;
+
+	end = 0;
+	while (dest[end])
+		dest++;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[end] = src[i];
+		end++;
+	}
+	dest[end] = '\n';
+	return (dest);
+
+
+}
