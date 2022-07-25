667048ab29d25ee2e4467a0af8d9c2461424bb61
ibrezma <imw31691@gmail.com>
2022-07-25 11:15:36 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+ *  * _puts_recursion - prints screen followed by new line
+ *    * @s: character string
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
+	else
+		putchar('\n');
+
+}
